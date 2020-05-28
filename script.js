3f6a5f17cc07d2d0fbd09b8a90a70ce0f2c2136d
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-28 17:49:39 +0200

    SettingUpRenderer

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function init() {
     scene = new THREE.Scene;
     camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight, 45 , 30000);
     camera.position.set(-900,-200,-900);
+
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 }
